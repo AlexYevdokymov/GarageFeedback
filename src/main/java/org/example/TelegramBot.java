3640e8b22c8b3ed64cb934d2fadb1c140fa018bf
olye <olexiy.yevdokimov.2000@gmail.com>
2025-09-17 13:44:55 +0300

    Position buttons added

--- src/main/java/org/example/TelegramBot.java
+++ src/main/java/org/example/TelegramBot.java
@@ -3,16 +3,25 @@ package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.methods.send.*;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.*;
 
 public class TelegramBot extends TelegramLongPollingBot {
     private String botToken;
     private String botUsername;
+
+    private static final Map<String, String> positions = Map.ofEntries(
+            Map.entry("mechanic", "Механік"),
+            Map.entry("manager", "Менеджер"),
+            Map.entry("electrician", "Електрик")
+    );
 
     public TelegramBot() {
         Properties props = new Properties();
@@ -42,13 +51,47 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             SendMessage message = new SendMessage();
             message.setChatId(chatId);
-            message.setText("Привіт, я бот");
-
+            message.setText("Привіт, обери свою посаду: ");
+            attachButtons(message, positions);
             try {
                 execute(message);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
+        if(update.hasCallbackQuery()) {
+            String data = update.getCallbackQuery().getData();
+            String chatId = update.getCallbackQuery().getMessage().getChatId().toString();
+
+            if (positions.containsKey(data)) {
+                SendMessage message = new SendMessage();
+                message.setChatId(chatId);
+                message.setText("Текст");
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+
+    public void attachButtons(SendMessage message, Map<String, String> buttons){
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : buttons.entrySet()) {
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(entry.getValue());
+            button.setCallbackData(entry.getKey());
+            keyboard.add(Collections.singletonList(button));
+        }
+
+        markup.setKeyboard(keyboard);
+        message.setReplyMarkup(markup);
+    }
+
 }
